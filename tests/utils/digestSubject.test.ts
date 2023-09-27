0fb4eed54beea43e87eb472ca38bb8a739beb992
David De Anda <omarandstuff@gmail.com>
2023-09-27 00:14:23 -0600

    Test refines, decribe actual symbols

--- tests/utils/digestSubject.test.ts
+++ tests/utils/digestSubject.test.ts
@@ -1,17 +1,15 @@
 import { digestSubject } from '../../src'
 
-describe('crypto-utils', (): void => {
-  describe('digestSubject', (): void => {
-    it('hashes a subject in the same way always depending the secret', async (): Promise<void> => {
-      const subject = 'this is a subject'
-      const digest1 = digestSubject(subject, 'secret')
-      const digest2 = digestSubject(subject, 'secret')
+describe(digestSubject, (): void => {
+  it('hashes a subject in the same way always depending the secret', async (): Promise<void> => {
+    const subject = 'this is a subject'
+    const digest1 = digestSubject(subject, 'secret')
+    const digest2 = digestSubject(subject, 'secret')
 
-      expect(digest1).toEqual(digest2)
+    expect(digest1).toEqual(digest2)
 
-      const digest3 = digestSubject(subject, 'secret-other')
+    const digest3 = digestSubject(subject, 'secret-other')
 
-      expect(digest1).not.toEqual(digest3)
-    })
+    expect(digest1).not.toEqual(digest3)
   })
 })
--- tests/utils/generateToken.test.ts
+++ tests/utils/generateToken.test.ts
@@ -6,34 +6,32 @@ beforeEach((): void => {
   jest.clearAllMocks()
 })
 
-describe('crypto-utils', (): void => {
-  describe('generateToken', (): void => {
-    it('generates an irrepealable random token', async (): Promise<void> => {
-      const generatedTokens: Set<string> = new Set()
+describe(generateToken, (): void => {
+  it('generates an irrepealable random token', async (): Promise<void> => {
+    const generatedTokens: Set<string> = new Set()
 
-      for (let i = 0; i < (process.env['CI'] ? 100_000 : 10_000); i++) {
-        const token = generateToken()
-
-        expect(token.length).toEqual(88)
-
-        generatedTokens.add(token)
-
-        expect(generatedTokens.size).toEqual(i + 1)
-      }
-    })
-
-    it('can take concern and/or a seed to generate the token based on context', async (): Promise<void> => {
-      jest.spyOn(crypto, 'randomBytes').mockImplementation((): Buffer => Buffer.from('123456789'))
-
+    for (let i = 0; i < (process.env['CI'] ? 100_000 : 10_000); i++) {
       const token = generateToken()
-      const tokenConcerned = generateToken({ concern: 'testing' })
-      const tokenSeeded = generateToken({ seed: 'machine-1' })
-      const tokenBoth = generateToken({ concern: 'testing', seed: 'machine-1' })
 
-      expect(token).not.toEqual(tokenConcerned)
-      expect(tokenConcerned).not.toEqual(tokenSeeded)
-      expect(tokenSeeded).not.toEqual(tokenBoth)
-      expect(tokenBoth).not.toEqual(token)
-    })
+      expect(token.length).toEqual(88)
+
+      generatedTokens.add(token)
+
+      expect(generatedTokens.size).toEqual(i + 1)
+    }
+  })
+
+  it('can take concern and/or a seed to generate the token based on context', async (): Promise<void> => {
+    jest.spyOn(crypto, 'randomBytes').mockImplementation((): Buffer => Buffer.from('123456789'))
+
+    const token = generateToken()
+    const tokenConcerned = generateToken({ concern: 'testing' })
+    const tokenSeeded = generateToken({ seed: 'machine-1' })
+    const tokenBoth = generateToken({ concern: 'testing', seed: 'machine-1' })
+
+    expect(token).not.toEqual(tokenConcerned)
+    expect(tokenConcerned).not.toEqual(tokenSeeded)
+    expect(tokenSeeded).not.toEqual(tokenBoth)
+    expect(tokenBoth).not.toEqual(token)
   })
 })
